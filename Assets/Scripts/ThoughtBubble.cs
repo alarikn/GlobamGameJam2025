989a2aedefa9c28ccbfb2c8cb0976c0dc27764ec
SerPoppari <topi.volanen@gmail.com>
2025-01-25 09:45:29 +0200

    Colors to thought bubble content
    Images in thought bubbles now have outlines.
    The thought bubble contents can now be checked, so that they change color to green or red.

--- Assets/Scripts/ThoughtBubble.cs
+++ Assets/Scripts/ThoughtBubble.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -10,6 +11,9 @@ public class ThoughtBubble : MonoBehaviour
     public GameObject image_prefab;
     public TextMeshProUGUI text;
 
+    private List<Image> instantated_images = new List<Image>();
+    int required_score = 0;
+
     public Sprite test_Sprite; // This is only for testing
 
     public void Start()
@@ -20,21 +24,60 @@ public class ThoughtBubble : MonoBehaviour
     public void newOrder(Sprite[] sprites, int score)
     {
         // Destroy all previous images
-        for (int i = image_panel.childCount - 1; i >= 0; i--)
+        for (int i = instantated_images.Count - 1; i >= 0; i--)
         {
-            Destroy(image_panel.GetChild(i).gameObject);
+            Destroy(instantated_images[i].gameObject);
         }
+        instantated_images.Clear();
 
         // Create new images
         for (int i = 0; i < sprites.Length; i++)
         {
+            GameObject new_outline_image = Instantiate(image_prefab);
+            new_outline_image.GetComponent<Image>().sprite = sprites[i];
+            new_outline_image.transform.SetParent(image_panel.transform);
+            new_outline_image.GetComponent<RectTransform>().sizeDelta = new Vector2(50f, 50f);
+            new_outline_image.transform.localScale = new Vector3(1f, 1f, 1f);
+            new_outline_image.GetComponent<Image>().color = Color.black;
+            instantated_images.Add(new_outline_image.GetComponent<Image>());
+
             GameObject new_image = Instantiate(image_prefab);
             new_image.GetComponent<Image>().sprite = sprites[i];
-            new_image.transform.SetParent(image_panel.transform);
-            new_image.transform.localScale = new Vector3(1f, 1f, 1f);
+            new_image.transform.SetParent(new_outline_image.transform);
+            new_image.GetComponent<RectTransform>().sizeDelta = new Vector2(50f, 50f);
+            new_image.transform.localScale = new Vector3(0.9f, 0.9f, 0.9f);
         }
 
         // Update the score
-        text.text = score.ToString();
+        required_score = score;
+        text.text = required_score.ToString();
+        text.color = Color.black;
+    }
+
+    public void checkOrder(int[] correct_image_indexes, int gotten_score)
+    {
+        // The "correct_image_indexes" can be changed
+        // Change color for all the image outlines
+        for (int i = 0; i < image_panel.childCount; i++)
+        {
+            if (correct_image_indexes.Contains(i))
+            {
+                instantated_images[i].color = Color.green;
+            }
+            else
+            {
+                instantated_images[i].color = Color.red;
+            }
+        }
+
+        // Change color for the score text
+        if (gotten_score >= required_score)
+        {
+            text.color = Color.green;
+        }
+        else
+        {
+            text.color = Color.red;
+        }
     }
 }
